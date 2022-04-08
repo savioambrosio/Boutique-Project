7a316cbdcad27f97ee3d38d19deb195e8c0fd6c9
Savio Ambrosio <savioambrosio2018@gmail.com>
2022-04-08 19:23:42 +0100

    first commit

---
+++ slide.js
@@ -0,0 +1,9 @@
+﻿  var imagens = ["Bou.jpg","lupa.png","icone.png"];
+var imgAtual = 0;
+	
+function trocaImg(){
+	imgAtual = (imgAtual + 1) %3;
+	document.getElementById("topImg").src = imagens[imgAtual];
+}
+setInterval(trocaImg, 4000);
+
